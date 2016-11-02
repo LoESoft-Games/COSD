f88f27bc2900480b9d488d55167e17a742a172bf
Devwarlt <devwarlt@loerealm.com>
2016-11-02 02:30:19 -0200

    Create Import common libraries.cs

---
+++ Import common libraries.cs
@@ -0,0 +1,8 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using wServer.logic.behaviors;
+using System.Threading.Tasks;
+using wServer.logic.transitions;
+using wServer.logic.loot;
